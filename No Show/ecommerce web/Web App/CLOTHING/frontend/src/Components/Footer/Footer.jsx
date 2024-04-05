39939532cd5980304084238f9eb2706324af5189
diakosiallen <diakosiallen@gmail.com>
2024-04-04 18:29:33 -0600

    Update Footer.jsx

--- No Show/ecommerce web/Web App/CLOTHING/frontend/src/Components/Footer/Footer.jsx
+++ No Show/ecommerce web/Web App/CLOTHING/frontend/src/Components/Footer/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
         <img src={footer_logo} alt="" width={350} />
       </div>
       <ul className="footer-links">
-        <li><Link to="/products">Products</Link></li>
+      
         <li><Link to="/about">About</Link></li>
         <li><Link to="/contact">Contact</Link></li>
       </ul>
