f99d1a6e75228df651ef0b5ad57e3c3a754387f1
Daniv2 <125231943+Daniv2@users.noreply.github.com>
2024-04-08 06:58:16 -0600

    Update Cart.jsx

--- No Show/ecommerce web/Web App/CLOTHING/frontend/src/Pages/Cart.jsx
+++ No Show/ecommerce web/Web App/CLOTHING/frontend/src/Pages/Cart.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import CartItems from '../Components/CartItems/CartItems';
-import { Navigate } from 'react-router-dom'; // Import Navigate
-import LoginNotification from '../Pages/LoginNotification'; // Import LoginNotification component
 
-// Load your Stripe publishable key
+import LoginNotification from '../Pages/LoginNotification'; 
+
+
 const stripePromise = loadStripe('pk_test_51OvSq1HjQsfCN3ihPmxsbkss0yEvWXDK0JSx6w9okn38UmB5l0OdRUWopK2KhigoBqGWy7w94HXiybMj6ywC3GI300Cl3WNXk7');
 
 const Cart = () => {
@@ -20,7 +20,6 @@ const Cart = () => {
 
   return (
     <div>
-      {/* Wrap your CartItems component with Elements */}
       <Elements stripe={stripePromise}>
         <CartItems />
       </Elements>
