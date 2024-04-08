a32dfa37b1ae1502d456b37ef6109b71621151bf
DavidA6969 <124728150+DavidA6969@users.noreply.github.com>
2024-04-08 08:56:59 -0400

    Update App.js

--- No Show/ecommerce web/Web App/CLOTHING/frontend/src/App.js
+++ No Show/ecommerce web/Web App/CLOTHING/frontend/src/App.js
@@ -1,4 +1,5 @@
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import Navbar from "./Components/Navbar/Navbar";
