ac3b94737779a7acc8c011dbd1fe6df858ad89cb
Daniv2 <125231943+Daniv2@users.noreply.github.com>
2024-04-12 21:49:29 -0600

    w

--- No Show/ecommerce web/Web App/CLOTHING/frontend/src/App.js
+++ No Show/ecommerce web/Web App/CLOTHING/frontend/src/App.js
@@ -12,6 +12,7 @@ import LoginSignup from "./Pages/LoginSignup";
 import Contact from "./Pages/Contact";
 import About from "./Pages/About";
 import Question from "./Pages/QuestionTab";
+import Profile from "./Profile/Profile.jsx";
 
 
 const stripePromise = loadStripe("pk_test_51OvSq1HjQsfCN3ihPmxsbkss0yEvWXDK0JSx6w9okn38UmB5l0OdRUWopK2KhigoBqGWy7w94HXiybMj6ywC3GI300Cl3WNXk7");
@@ -36,6 +37,7 @@ function App() {
               </Elements>
             }
           />
+          <Route path="/profile" component={Profile} />
           <Route path="/login" element={<LoginSignup />} />
           <Route path='/contact' element={<Contact />} />
           <Route path='/about' element={<About />} />
---
+++ No Show/ecommerce web/Web App/CLOTHING/frontend/src/Profile/Profile.jsx
@@ -0,0 +1,13 @@
+// Profile.js
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div>
+      <h1>Profile</h1>
+      {/* Add your profile information here */}
+    </div>
+  );
+};
+
+export default Profile;
