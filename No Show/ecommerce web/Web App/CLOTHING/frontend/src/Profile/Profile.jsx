63eadb4ecfb951ce8d614d19b695f90dfdb902e3
Daniv2 <125231943+Daniv2@users.noreply.github.com>
2024-04-14 13:35:16 -0600

    Update Profile.jsx

--- No Show/ecommerce web/Web App/CLOTHING/frontend/src/Profile/Profile.jsx
+++ No Show/ecommerce web/Web App/CLOTHING/frontend/src/Profile/Profile.jsx
@@ -1,3 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './Profile.css';
 
+const Profile = () => {
+  const [name, setName] = useState('');
+  const [gender, setGender] = useState('');
+  const [bio, setBio] = useState('');
+  const [profilePicture, setProfilePicture] = useState(null);
+  const [savedProfile, setSavedProfile] = useState(null);
+  const [isEditingName, setIsEditingName] = useState(false);
+  const [isEditingGender, setIsEditingGender] = useState(false);
+  const [isEditingBio, setIsEditingBio] = useState(false);
