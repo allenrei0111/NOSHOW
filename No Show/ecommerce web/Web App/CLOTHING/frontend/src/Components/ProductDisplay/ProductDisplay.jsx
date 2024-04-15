951513a2966b417ab4f3f41774b444cd80f2089c
DavidA6969 <124728150+DavidA6969@users.noreply.github.com>
2024-04-15 01:51:21 -0400

    Update ProductDisplay.jsx

--- No Show/ecommerce web/Web App/CLOTHING/frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ No Show/ecommerce web/Web App/CLOTHING/frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -125,7 +125,11 @@ const ProductDisplay = ({ product }) => {
         ) : (
           <button onClick={() => handleAddToCart(product.id)}>ADD TO CART</button>
         )}
-       
+         {addedToFavorite ? (
+        <p>ADDED TO FAVORITES</p>
+      ) : (
+        <button onClick={() => handleAddToFavorite(product.id)}>ADD TO FAVORITES</button>
+      )}
       </div>
     </div>
   );
