4bb303907da978e1a0960b98d275de300007f654
diakosiallen <diakosiallen@gmail.com>
2024-04-15 00:13:39 -0600

    Create TermsOfService.js

---
+++ No Show/ecommerce web/Web App/CLOTHING/frontend/src/Pages/TermsOfService.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './CSS/TermsOfService.css';
+
+const TermsOfService = () => {
+  return (
+    <div className="terms-of-service">
+      <h2>Terms of Service</h2>
+      <p>Insert your terms of service content here...</p>
+    </div>
+  );
+};
+
+export default TermsOfService;
