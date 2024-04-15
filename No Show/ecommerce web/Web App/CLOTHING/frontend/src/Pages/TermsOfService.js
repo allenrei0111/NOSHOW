65d65e2030500bae5d769e096024b3e31245cb8c
diakosiallen <diakosiallen@gmail.com>
2024-04-15 00:45:41 -0600

    Update TermsOfService.js

--- No Show/ecommerce web/Web App/CLOTHING/frontend/src/Pages/TermsOfService.js
+++ No Show/ecommerce web/Web App/CLOTHING/frontend/src/Pages/TermsOfService.js
@@ -14,7 +14,11 @@ const TermsOfService = () => {
         <p>We use cookies in accordance with our Privacy Policy. By using our website, you consent to the use of cookies.</p>
       </section>
 
-      
+      <section className="section">
+        <h3 className="subtitle">License</h3>
+        <p>All content on LanderStylez is owned by us. Do not republish without permission.</p>
+        <p>You may not use our materials for any commercial purpose without obtaining a license.</p>
+      </section>
 
 
 
