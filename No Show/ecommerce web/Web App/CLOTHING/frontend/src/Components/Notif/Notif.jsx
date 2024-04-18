3023754840c1eaa666b9f2e3130b5be329a819eb
DavidA6969 <124728150+DavidA6969@users.noreply.github.com>
2024-04-18 13:33:56 -0400

    d

--- No Show/ecommerce web/Web App/CLOTHING/frontend/src/Components/Notif/Notif.jsx
+++ No Show/ecommerce web/Web App/CLOTHING/frontend/src/Components/Notif/Notif.jsx
@@ -5,7 +5,6 @@ const Notif = ({ showNotification, handleClose, cartItemCount, paymentNotificati
     <div className={`notification ${showNotification ? "show" : ""}`}>
       <button onClick={handleClose}>
         <svg
-          xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
           width="24"
           height="24"
@@ -13,17 +12,16 @@ const Notif = ({ showNotification, handleClose, cartItemCount, paymentNotificati
           <path fill="none" d="M0 0h24v24H0z"></path>
           <path
             fill="currentColor"
-            d="M20 17h2v2H2v-2h2v-7a8 8 0 1 1 16 0v7zm-2 0v-7a6 6 0 1 0-12 0v7h12zm-9 4h6v2H9v-2z"
           ></path>
         </svg>
       </button>
-      <span className="cart-item-count">{cartItemCount}</span> {/* Display cart item count */}
-      {paymentNotification && ( // Display payment notification if available
+      <span className="cart-item-count">{cartItemCount}</span> 
+      {paymentNotification && (
         <div className="payment-notification">
           Payment of ${paymentNotification.amount} successful!
         </div>
       )}
-      {notificationCount > 0 && ( // Display general notification count if available
+      {notificationCount > 0 && ( 
         <div className="general-notification">
           You have {notificationCount} new notifications
         </div>
